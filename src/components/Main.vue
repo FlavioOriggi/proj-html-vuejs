0f869d4cb05ea11a6b066ed3bddc8c5ea2e4da74
Flavio <flavio.origgi@outlook.it>
2021-07-19 20:36:09 +0200

    terminata sezione Real World Results

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -69,8 +69,7 @@
                             <div class="i-red">                      
                                 <i class="fab fa-cloudversify"></i>
                             </div> 
-                        </div> 
-                                                                      
+                        </div>                                                                       
                         <h2>Flexibility e Rest</h2>
                         <img class="img-fusion" src="@/assets/divider-x-red.png" alt="">
                         <p class="p-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
@@ -81,8 +80,7 @@
                             <div class="i-red">
                                 <i class="fas fa-heartbeat"></i>
                             </div> 
-                        </div>                       
-                                                                        
+                        </div>                                                               
                         <h2>Health e Diet</h2>
                         <img class="img-fusion" src="@/assets/divider-x-red.png" alt="">
                         <p class="p-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
@@ -90,6 +88,67 @@
                     
                 </div>
             </div>         
+        </section>
+
+        <!-- sezione REAL WORLD RESULTS -->
+        <section class="rlw bcg-lightgrey">
+            <div class="container-xl"> 
+                <div class="row">
+                    <div class="col text-rlw text-center">
+                        <h2>real words results</h2>
+                        <img class="img-wcf" src="@/assets/divider-xx-red.png" alt=""> 
+                    </div>
+                </div>
+
+                
+                <div class="row g-1">
+                    <div class="col-6 results-contenent ">
+                        <div class="results-item d-flex ">
+                            <div>
+                                <img class="img-wcf" src="@/assets/review_1-compressor.jpg" alt="">
+                            </div>
+                            <div class="text-results">
+                                <span>“The trainers at Avada Gym have helped me reach my fitness goals beyond anything I    could of hoped for. I love it!”</span>
+                                <p>– Tara Smith</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 results-contenent ">
+                        <div class="results-item d-flex">
+                            <div>
+                                <img class="img-wcf" src="@/assets/review_3-compressor-2.jpg" alt="">
+                            </div>
+                            <div class="text-results">
+                                <span>“The atmosphere at Avada Gym is amazing, the people are even better. All in all, it   is a great place to work out.”</span>
+                                <p>– Simon Chel</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 results-contenent  ">
+                        <div class="results-item d-flex">
+                            <div>
+                                <img class="img-wcf" src="@/assets/review_2-compressor.jpg" alt="">
+                            </div>
+                            <div class="text-results">
+                                <span>“The facilities at Avada Gym stood out to me the most. They are well maintained and have high quality equipment.”</span>
+                                <p>– Jen Wirth</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6  results-contenent ">
+                        <div class="results-item d-flex">
+                            <div>
+                                <img class="img-wcf" src="@/assets/review_4-compressor-2.jpg" alt="">
+                            </div>
+                            <div class="text-results">
+                                <span>“No bad things to say about Avada Gym, they are top notch at every corner and truly take care of their customers.”</span>
+                                <p>– Jeff Glum</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
         </section>
     </main>
 </template>
@@ -115,6 +174,7 @@ main{
             font-size: 2.5rem;
             font-weight: bold;
             color: white;
+           
         }
 
         .img-wcf{
@@ -181,7 +241,9 @@ main{
         padding-right: 30px;
         padding-bottom: 110px;
 
-        .item-fusion{          
+        .item-fusion{   
+            
+            // manca effetto immagine
     
             h2{
                 font-size: 1.2rem;
@@ -203,6 +265,54 @@ main{
 
         
 
+    }
+
+    .rlw{
+        padding-top: 140px;
+        padding-left: 30px;
+        padding-right: 30px;
+        padding-bottom: 130px;
+
+        .text-rlw{
+            h2{
+                line-height: 1.28;
+                font-size: 2.5rem;
+                font-weight: bold;
+                color: white;
+                text-transform: uppercase;
+            }
+
+            .img-wcf{
+                width: 30px;
+                margin: 30px 0;            
+            }
+        }
+
+        .results-contenent{
+            padding: 0 25px;
+            margin-bottom: 40px;            
+
+            .results-item{
+
+                padding: 30px;
+                background-color: #1a1c1e;
+
+                .text-results{
+                    padding-left: 30px;
+                    
+                    span{
+                        line-height: 2.07;
+                        color: #aaa9a9;
+                    } 
+                    
+                    p{
+                        color: #aaa9a9;
+                    }
+                }
+            }
+
+            
+        }
     }
 
 
