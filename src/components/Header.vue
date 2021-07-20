b226de35cd24bf1a3a2fda49245cfaa831ce4adc
Flavio <flavio.origgi@outlook.it>
2021-07-20 11:03:32 +0200

    add jumboMain + card HPF

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-    <header class="jumbo"> 
+    <header class="jumbo">
+
+        <!-- sezione header + navbar   -->
         <div class="headernav"> 
-            <div class="row">
-                
-                
+            <div class="row">                
                 <div class="col-3 logo-contenent">
                     <a class="" href="https://avada.theme-fusion.com/gym/membership/"><img src="@/assets/gym_logo_1x.png" alt=""></a>
 
@@ -19,33 +19,21 @@
                     <button class="bcg-btn-orange" ><a href="https://avada.theme-fusion.com/gym/membership/" class="text_nounderline text-uppercase text-light">join us today!</a></button>
 
                 </div>
-                
             </div>
         </div>
-
         
-              
-            <div class="text-jumbo text-center text-light">
-                <div class="row">
-                    <div class="col">
-                        <h1 class="text-uppercase fw-bold">no pain no gain</h1>
-                        <h2>Motivation is what gets you started. Habit is what keeps you going.</h2>
-                        <div class="btn-jumbo">
-                            <button class="bcg-btn-trasparent" ><a href="https://avada.theme-fusion.com/gym/membership/" class="text_nounderline text-uppercase">join us today</a></button>
-                        </div>
+        <!-- sezione jumbo   -->
+        <div class="text-jumbo text-center text-light">
+            <div class="row ">
+                <div class="col">
+                    <h1 class="text-uppercase fw-bold">no pain no gain</h1>
+                    <h2>Motivation is what gets you started. Habit is what keeps you going.</h2>
+                    <div class="btn-jumbo">
+                        <button class="bcg-btn-trasparent" ><a href="https://avada.theme-fusion.com/gym/membership/" class="text_nounderline text-uppercase">join us today</a></button>
                     </div>
-                    
-                    
-
-
-                </div> 
-            </div>  
-        
-        
-                       
-         
-        
-        
+                </div>
+            </div> 
+        </div> 
     </header>
 </template>
 
@@ -87,7 +75,7 @@ export default {
 .jumbo{
     background-image: url('../assets/home-banner.jpg');
     background-size: cover;
-    height: 580px;
+    height: 100vh;
 
     .headernav{
         padding: 30px 70px;
@@ -115,15 +103,12 @@ export default {
             padding: 40px 0;
         } 
     }
-   
 
-    
-    
-    
-    .text-jumbo{
+    .text-jumbo{        
         h1{
             font-size: 7rem;                      
-            line-height: 1;          
+            line-height: 1;   
+            margin-top: 10%;       
         }
 
         h2{
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,11 +4,11 @@
         <section class="w-class-fit text-center bcg-lightgrey"> 
             <div class="container-xl"> 
                 <div class="row">             
-                    <h2>WORLD CLASS Fitness</h2>
+                    <h2 class="title-section-h2">WORLD CLASS Fitness</h2>
                     <div>
-                        <img class="img-wcf" src="@/assets/divider-xx-red.png" alt="">  
-                    </div>                    
-                    <p class="paragraph-wcf p-light-gray">At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we’re equipped to help you reach your goals of a healthy and fit lifestyle.</p>
+                        <img class="double-x" src="@/assets/divider-xx-red.png" alt="">  
+                    </div>                     
+                    <p class="paragraph-wcf text-light-gray">At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we’re equipped to help you reach your goals of a healthy and fit lifestyle.</p>
                 </div>  
                 <div class="divider-candy"></div>              
             </div> 
@@ -25,11 +25,11 @@
                     <form class="col-6 form-item">
                         <h3 class="text-light fw-bold mb-5">Sign Up For A Free Training Session</h3>                            
                         <div class="mb-5">
-                            <p class="p-light-gray">Name* </p>
+                            <p class="text-light-gray">Name* </p>
                             <input type="text" class="form-control bcg-lightgrey border border-light py-3">
                         </div>                        
                         <div class="mb-5">
-                            <p class="p-light-gray">Email</p>
+                            <p class="text-light-gray">Email</p>
                             <input type="text" class="form-control bcg-lightgrey border border-light py-3" >
                         </div>                        
                         <button><a href="#" class="btn-form button-xlarge bcg-btn-orange text_nounderline text-uppercase text-light">start now</a></button>      
@@ -50,7 +50,7 @@
                         </div> 
                         <h2 class="mb-2">Strength e Conditioning</h2>                                               
                         <img class="img-fusion mb-2" src="@/assets/divider-x-red.png" alt="">
-                        <p class="p-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
+                        <p class="text-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
                     </div> 
 
                     <div class="col-3 item-fusion text-center"> 
@@ -61,7 +61,7 @@
                         </div>                                           
                         <h2>Fitness e Cardio</h2>
                         <img class="img-fusion" src="@/assets/divider-x-red.png" alt="">
-                        <p class="p-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
+                        <p class="text-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
                     </div> 
 
                     <div class="col-3 item-fusion text-center"> 
@@ -72,7 +72,7 @@
                         </div>                                                                       
                         <h2>Flexibility e Rest</h2>
                         <img class="img-fusion" src="@/assets/divider-x-red.png" alt="">
-                        <p class="p-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
+                        <p class="text-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
                     </div> 
 
                     <div class="col-3 item-fusion text-center"> 
@@ -83,7 +83,7 @@
                         </div>                                                               
                         <h2>Health e Diet</h2>
                         <img class="img-fusion" src="@/assets/divider-x-red.png" alt="">
-                        <p class="p-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
+                        <p class="text-light-gray">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In augue nisl, ornare es volutpat etra us.</p>
                     </div>                   
                     
                 </div>
@@ -91,58 +91,57 @@
         </section>
 
         <!-- sezione REAL WORLD RESULTS -->
-        <section class="rlw bcg-lightgrey">
+        <section class="rwr bcg-lightgrey">
             <div class="container-xl"> 
                 <div class="row">
-                    <div class="col text-rlw text-center">
-                        <h2>real words results</h2>
-                        <img class="img-wcf" src="@/assets/divider-xx-red.png" alt=""> 
+                    <div class="col text-rwr text-center">
+                        <h2 class="title-section-h2 text-uppercase">real words results</h2>
+                        <img class="double-x" src="@/assets/divider-xx-red.png" alt=""> 
                     </div>
                 </div>
-
                 
                 <div class="row g-1">
-                    <div class="col-6 results-contenent ">
-                        <div class="results-item d-flex ">
+                    <div class="col-6 card-contenent">
+                        <div class="card-item d-flex mb-4">
                             <div>
-                                <img class="img-wcf" src="@/assets/review_1-compressor.jpg" alt="">
+                                <img class="" src="@/assets/review_1-compressor.jpg" alt="">
                             </div>
-                            <div class="text-results">
-                                <span>“The trainers at Avada Gym have helped me reach my fitness goals beyond anything I    could of hoped for. I love it!”</span>
-                                <p>– Tara Smith</p>
+                            <div class="text-card">
+                                <span class="text-light-gray">“The trainers at Avada Gym have helped me reach my fitness goals beyond anything I    could of hoped for. I love it!”</span>
+                                <p class="text-light-gray">– Tara Smith</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 results-contenent ">
-                        <div class="results-item d-flex">
+                    <div class="col-6 card-contenent">
+                        <div class="card-item d-flex mb-4">
                             <div>
-                                <img class="img-wcf" src="@/assets/review_3-compressor-2.jpg" alt="">
+                                <img class="" src="@/assets/review_3-compressor-2.jpg" alt="">
                             </div>
-                            <div class="text-results">
-                                <span>“The atmosphere at Avada Gym is amazing, the people are even better. All in all, it   is a great place to work out.”</span>
-                                <p>– Simon Chel</p>
+                            <div class="text-card">
+                                <span class="text-light-gray">“The atmosphere at Avada Gym is amazing, the people are even better. All in all, it   is a great place to work out.”</span>
+                                <p class="text-light-gray">– Simon Chel</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 results-contenent  ">
-                        <div class="results-item d-flex">
+                    <div class="col-6 card-contenent">
+                        <div class="card-item d-flex mb-5">
                             <div>
-                                <img class="img-wcf" src="@/assets/review_2-compressor.jpg" alt="">
+                                <img class="" src="@/assets/review_2-compressor.jpg" alt="">
                             </div>
-                            <div class="text-results">
-                                <span>“The facilities at Avada Gym stood out to me the most. They are well maintained and have high quality equipment.”</span>
-                                <p>– Jen Wirth</p>
+                            <div class="text-card">
+                                <span class="text-light-gray">“The facilities at Avada Gym stood out to me the most. They are well maintained and have high quality equipment.”</span>
+                                <p class="text-light-gray">– Jen Wirth</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6  results-contenent ">
-                        <div class="results-item d-flex">
+                    <div class="col-6  card-contenent">
+                        <div class="card-item d-flex mb-5">
                             <div>
-                                <img class="img-wcf" src="@/assets/review_4-compressor-2.jpg" alt="">
+                                <img class="" src="@/assets/review_4-compressor-2.jpg" alt="">
                             </div>
-                            <div class="text-results">
-                                <span>“No bad things to say about Avada Gym, they are top notch at every corner and truly take care of their customers.”</span>
-                                <p>– Jeff Glum</p>
+                            <div class="text-card">
+                                <span class="text-light-gray">“No bad things to say about Avada Gym, they are top notch at every corner and truly take care of their customers.”</span>
+                                <p class="text-light-gray">– Jeff Glum</p>
                             </div>
                         </div>
                     </div>
@@ -150,6 +149,71 @@
                 
             </div>
         </section>
+
+        <!-- sezione HIGH PERFORMANCE FACILITIES -->
+        <section class="hpf bcg-lightgrey">
+            <div class="container-xl"> 
+                <div class="row">
+                    <div class="col text-rwr text-center">
+                        <h2 class="title-section-h2 text-uppercase">high performances facilities</h2>
+                        <img class="double-x" src="@/assets/divider-xx-red.png" alt=""> 
+                    </div>
+                </div>
+                
+                <div class="row g-1">
+                    <div class="col-4 card-contenent ">
+                        <div class="card-item ">                            
+                            <img class="img-wcf" src="@/assets/home-image1.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-4 card-contenent ">
+                        <div class="card-item"> 
+                            <img class="img-wcf" src="@/assets/home-image2.jpg" alt="">  
+                        </div>
+                    </div>
+                    <div class="col-4 card-contenent  ">
+                        <div class="card-item">
+                            <img class="img-wcf" src="@/assets/home-image3.jpg" alt="">  
+                        </div>
+                    </div>
+                    <div class="col-4  card-contenent ">
+                        <div class="card-item">                            
+                            <img class="img-wcf" src="@/assets/home-image4.jpg" alt="">                            
+                        </div>
+                    </div>
+                    <div class="col-4  card-contenent ">
+                        <div class="card-item">                            
+                            <img class="img-wcf" src="@/assets/home-image54.jpg" alt="">                            
+                        </div>
+                    </div>
+                    <div class="col-4  card-contenent ">
+                        <div class="card-item">                            
+                            <img class="img-wcf" src="@/assets/home-image6.jpg" alt="">                            
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+
+        <!-- sezione MAIN JUMBO -->
+        <section class="main-jumbo">
+            <div class="container-xl"> 
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-8 text-jumbo text-light text-center">
+                        <span>“ I fear not the man who has practiced 10,000 <br> kicks once, but I fear the man who has <br> practiced one kick 10,000 times ”</span>
+                        <p class="text-uppercase fw-bold py-3">bruce lee • quote of the day</p>
+                    </div>
+                    <div class="col-2"></div>
+
+
+
+
+
+                </div>
+            </div>
+        </section>           
     </main>
 </template>
 
@@ -167,27 +231,13 @@ main{
         padding-top: 140px;
         padding-left: 30px;
         padding-right: 30px;
-        padding-bottom: 110px;
-
-        h2{
-            line-height: 1.28;
-            font-size: 2.5rem;
-            font-weight: bold;
-            color: white;
-           
-        }
-
-        .img-wcf{
-            width: 30px;
-            margin: 30px 0;            
-        }
+        padding-bottom: 110px;       
 
         .paragraph-wcf{
             margin-bottom: 20px;
             padding: 0 175px;
             font-size: 1.25rem;            
         }
-
         .divider-candy{
             position: relative;
             left: 48.5%;
@@ -201,11 +251,8 @@ main{
         }
     }
 
-    .form-container {      
-        padding-left: 30px;
-        padding-right: 30px;
-        padding-top: 140px;
-        padding-bottom: 70px;
+    .form-container {  
+        padding: 140px 30px 70px 30px;  
 
         .img-contenent{ 
             padding: 0 25px;          
@@ -224,8 +271,7 @@ main{
             
             .form-control.active{
                 background-color: #25292b                
-            }
-            
+            }            
             .btn-form{
                 width: 100px;
             }
@@ -236,10 +282,7 @@ main{
         background-image: url('../assets/gym_bkgd_bw-compressor.jpg');
         background-size: cover;
         background-position: center;
-        padding-top: 140px;
-        padding-left: 30px;
-        padding-right: 30px;
-        padding-bottom: 110px;
+        padding: 140px 30px 110px 30px;        
 
         .item-fusion{   
             
@@ -256,63 +299,59 @@ main{
                     cursor: pointer;
                 }  
             }
-
         }
-
-        
-        
-        
-
-        
-
     }
 
-    .rlw{
-        padding-top: 140px;
-        padding-left: 30px;
-        padding-right: 30px;
-        padding-bottom: 130px;
+    .rwr{
+        padding: 130px 30px 130px 30px;              
 
-        .text-rlw{
-            h2{
-                line-height: 1.28;
-                font-size: 2.5rem;
-                font-weight: bold;
-                color: white;
-                text-transform: uppercase;
-            }
-
-            .img-wcf{
-                width: 30px;
-                margin: 30px 0;            
-            }
-        }
-
-        .results-contenent{
-            padding: 0 25px;
-            margin-bottom: 40px;            
-
-            .results-item{
-
-                padding: 30px;
+        .card-contenent{                     
+            .card-item{                
                 background-color: #1a1c1e;
-
                 .text-results{
                     padding-left: 30px;
                     
                     span{
-                        line-height: 2.07;
-                        color: #aaa9a9;
-                    } 
-                    
-                    p{
-                        color: #aaa9a9;
+                        line-height: 2.07;                        
                     }
                 }
-            }
-
-            
+            }            
         }
+    }
+
+    .hpf{
+        padding: 150px 30px 150px 30px;
+        .card-contenent{
+            .card-item{
+                img{
+                    &:hover{                    
+                        transform: scale(1.05); // da migliorare leffetto
+                        box-shadow: 0 3px 15px rgba(0,0,0,.4);
+                        cursor: pointer;
+                    }
+                }                
+            }
+        }
+    }
+
+    .main-jumbo{
+        background-image: url('../assets/home-testimonial-bg.jpg');
+        background-size: cover;
+        background-position: center;
+        padding: 170px 30px 140px 30px;
+        .text-jumbo{
+            span{
+                font-size: 2.25rem;
+                line-height: 3.5rem;                
+            } 
+            p{
+                letter-spacing: 2px;
+                font-size: 1.25rem;
+
+
+            }          
+        }
+
     }
 
 
